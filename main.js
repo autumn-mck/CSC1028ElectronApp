675f4e2faeddad7a4d252c448a2b0d9140f3b2e1
Autumn McKee <autumn@mck.is>
2022-03-22 22:09:39 +0000

    Updating comments

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
-// include the Node.js 'path' module at the top of your file
+// Node.js to launch the electron app
+// Mostly from https://www.electronjs.org/docs/latest/tutorial/quick-start/
+
 const path = require("path");
 const { app, BrowserWindow } = require("electron");
 
-// modify your existing createWindow() function
+// Function for creating the window
 const createWindow = () => {
 	const win = new BrowserWindow({
 		width: 800,
@@ -13,17 +15,21 @@ const createWindow = () => {
 		},
 	});
 
+	// Load the index.html file
 	win.loadFile("index.html");
 };
 
+// When the app is ready, create the window
 app.whenReady().then(() => {
 	createWindow();
 
+	// Apparently a workaround for odd MacOS behaviour
 	app.on("activate", () => {
 		if (BrowserWindow.getAllWindows().length === 0) createWindow();
 	});
 });
 
+// Quit the application if no windows are left open
 app.on("window-all-closed", () => {
 	if (process.platform !== "darwin") app.quit();
 });
